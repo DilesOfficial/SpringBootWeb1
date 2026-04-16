082c720770d7d7affd21b590ba8252ab75ac31c2
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-16 23:39:16 +0530

    First

--- src/main/java/com/dilusha/springbootweb1/SpringBootWeb1Application.java
+++ src/main/java/com/dilusha/springbootweb1/SpringBootWeb1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootWeb1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWeb1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWeb1Application.class, args);
+    }
 
 }
