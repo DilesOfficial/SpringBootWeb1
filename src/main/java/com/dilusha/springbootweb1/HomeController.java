2db907f8af6a2334229cdc80872f28552a4a6b2c
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-17 13:20:12 +0530

    Using ModelAttribute

--- src/main/java/com/dilusha/springbootweb1/HomeController.java
+++ src/main/java/com/dilusha/springbootweb1/HomeController.java
@@ -2,12 +2,18 @@ package com.dilusha.springbootweb1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+
+    @ModelAttribute("course")
+    public String courseName(){
+        return "Java";
+    }
 
     @RequestMapping("/")
     public String home() {
@@ -26,15 +32,15 @@ public class HomeController {
     }
 
     @RequestMapping("addAlien")
-    public ModelAndView addAlien(@RequestParam("aid") int aid, @RequestParam("aname") String aname, ModelAndView mv) {
+    public String addAlien(Alien alien) {
 
-        Alien alien = new Alien();
-        alien.setAid(aid);
-        alien.setAname(aname);
+//        Alien alien = new Alien();
+//        alien.setAid(aid);
+//        alien.setAname(aname);
 
-        mv.addObject("alien", alien);
-        mv.setViewName("results");
+//        mv.addObject("alien", alien);
+//        mv.setViewName("results");
 
-        return mv;
+        return "results";
     }
 }
